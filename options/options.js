86981cdc6e312b77a5df9039103caf36f4d51ab7
Crowdbotics <team@crowdbotics.com>
2022-02-12 04:09:58 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "mortgage_quiz_33561",
+  url: "https://mortgage_quiz_33561.botics.co",
+  api: "https://mortgage_quiz_33561.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
